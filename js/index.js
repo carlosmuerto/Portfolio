2adfeebdf03741df0771b4660c0491da48b4ef79
Carlos Vivas <carlosmuerto@gmail.com>
2022-07-19 13:19:01 -0400

    Fix nav Menu open when in desktop

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 // Mobile menu
 
 const navbarUl = document.querySelector('nav.navbar > ul');
+const navbarTitle = document.querySelector('nav.navbar > a#welcome');
 const navbarMenuBtn = document.querySelector('nav.navbar > a#menu-btn');
 const navbarMenuBtnClose = document.querySelectorAll(
   'nav.navbar > ul > li > a',
 );
 
+function getStyle(element, name) {
+  const ComputedStyle = window.getComputedStyle
+    ? window.getComputedStyle(element, null).getPropertyValue(name) : null;
+  return element.currentStyle ? element.currentStyle[name] : ComputedStyle;
+}
+
 function showNavUl() {
+  if (getStyle(navbarTitle, 'display') === 'none') return;
+
   if (navbarUl.classList.contains('show')) {
     navbarUl.classList.remove('show');
   } else {
