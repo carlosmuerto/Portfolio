baa2f93a2842ada5892bd945bc6378b3dd512045
Odette Gleen <nkargleeny@gmail.com>
2022-07-19 23:49:52 +0100

    Add popup functionality

--- js/index.js
+++ js/index.js
@@ -22,8 +22,86 @@ function showNavUl() {
     navbarUl.classList.add('show');
   }
 }
+// popup modal
+
+function buildWork(
+  title,
+  description,
+  featured,
+  technologies,
+  linkLive,
+  linkSource,
+) {
+  return {
+    title,
+    description,
+    featured,
+    technologies,
+    linkLive,
+    linkSource,
+  };
+}
+function BuildGenericWork(title) {
+  return buildWork(
+    title,
+    `${title} Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolor error nam deleniti tempora excepturi iure animi aut, repellat amet aliquam illo quos eaque? Officiis ullam accusamus est soluta obcaecati?`,
+    'assets/works/Snapshoot-Portfolio.png',
+    [
+      'Ruby on rails',
+      'css',
+      'JavaScript',
+      'html',
+    ],
+    'https://carlosmuerto.github.io/Portfolio/',
+    'https://github.com/carlosmuerto/Portfolio',
+  );
+}
+const works = [
+  BuildGenericWork('Multi-Post Stories Gain+Glory 01'),
+  BuildGenericWork('Multi-Post Stories Gain+Glory 02'),
+  BuildGenericWork('Multi-Post Stories Gain+Glory 03'),
+];
+const modal = document.querySelector('#modal');
+const showModalBtn = document.querySelectorAll('#works .card-body button');
+const modalCloser = document.querySelectorAll('#modal .close-modal');
+function modifyModal(work) {
+  modal.querySelector('.card .name h3').textContent = work.title;
+  modal.querySelector('.card .featured-image img').src = work.featured;
+  modal.querySelector('.card .technologies ul').textContent = '';
+  work.technologies.forEach((element) => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(element));
+    modal.querySelector('.card .technologies ul').appendChild(li);
+  });
+  modal.querySelector('.card .description p').textContent = work.description;
+  modal.querySelector('.card .buttons a#see-live-ver');
+}
+function showModal(index) {
+  console.log(works[index]);
+  modifyModal(works[index]);
+  if (modal.classList.contains('show')) {
+    modal.classList.remove('show');
+  } else {
+    modal.classList.add('show');
+  }
+}
+function initModal() {
+  showModalBtn.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      showModal(index);
+    });
+  });
+  modalCloser.forEach((element, index) => {
+    element.addEventListener('click', () => {
+      showModal(index);
+    });
+  });
+}
+
+// init funtion
 
 function init() {
+   initModal() 
   navbarMenuBtn.addEventListener('click', showNavUl);
   navbarMenuBtnClose.forEach((element) => {
     element.addEventListener('click', showNavUl);
